ae569a6f6a0dc83553247cbca60c5b7980dabdc4
sonal.nanodkar <sonal.nanodkar@kilowott.com>
2023-03-30 12:59:02 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SYSPROWCFServicesClientLibrary40;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test Syspro DLL..!!");
+
+        SYSPROWCFServicesPrimitiveClient client = new SYSPROWCFServicesPrimitiveClient("net.tcp://APPSERVER:31001/SYSPROWCFService/Soap", SYSPROWCFBinding.NetTcp);
+
+        Console.WriteLine("Version: ");
+        client.GetVersion();
+        Console.ReadLine();
+
+        Console.WriteLine("Service Name: ");
+        client.GetServiceName();
+        Console.ReadLine();
+
+    }
+}
+ 

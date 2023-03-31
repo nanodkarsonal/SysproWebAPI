7a86fec8f43b042aa180e54fe62fe7ff4fd7eb07
sonal.nanodkar <sonal.nanodkar@kilowott.com>
2023-03-31 12:56:50 +0530

    updated base address

--- Program.cs
+++ Program.cs
@@ -9,15 +9,17 @@ class Program
     {
         Console.WriteLine("Test Syspro DLL..!!");
 
-        SYSPROWCFServicesPrimitiveClient client = new SYSPROWCFServicesPrimitiveClient("net.tcp://APPSERVER:31001/SYSPROWCFService/Soap", SYSPROWCFBinding.NetTcp);
-
-        Console.WriteLine("Version: ");
-        client.GetVersion();
+        // SYSPROWCFServicesPrimitiveClient client = new SYSPROWCFServicesPrimitiveClient("net.tcp://APPSERVER:31001/SYSPROWCFService/Soap", SYSPROWCFBinding.NetTcp);
+        SYSPROWCFServicesPrimitiveClient client = new SYSPROWCFServicesPrimitiveClient("net.tcp://localhost:91/SYSPROWCFService/Soap", SYSPROWCFBinding.NetTcp);
+        var version = client.GetVersion();
+        Console.WriteLine("Version: " + version);
         Console.ReadLine();
 
         Console.WriteLine("Service Name: ");
-        client.GetServiceName();
+        var name = client.GetServiceName();
+        Console.WriteLine(name);
         Console.ReadLine();
+
 
     }
 }
